0baeb24856a4cde76bbbcabe90088c75e4f96189
vitalets <vitalets@yandex-team.ru>
2017-07-17 14:15:54 +0300

    add pending test for onMessage

--- test/index.test.js
+++ test/index.test.js
@@ -237,11 +237,21 @@ describe('WebSocketAsPromised', function () {
   });
 
   describe('onMessage', function () {
-    it('should dispatch data', function () {
+    it('should dispatch data after JSON response', function () {
       const wsp = new WebSocketAsPromised(this.url, {createWebSocket});
       const res = new Promise(resolve => {
         wsp.onMessage.addListener(resolve);
         wsp.open().then(() => wsp.request({foo: 'bar'}));
+      });
+      return assert.eventually.propertyVal(res, 'foo', 'bar');
+    });
+
+    // todo: find way to write this test. Currently it fails because mocha listens window.onerror
+    it.skip('should dispatch data after string request', function () {
+      const wsp = new WebSocketAsPromised(this.url, {createWebSocket});
+      const res = new Promise(resolve => {
+        wsp.onMessage.addListener(resolve);
+        wsp.open().then(() => wsp.request({nonJSONResponse: true})).catch(() => {});
       });
       return assert.eventually.propertyVal(res, 'foo', 'bar');
     });
