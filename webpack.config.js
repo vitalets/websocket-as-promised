933d5e283938c36d03e3a5f372920245ea3c1e3e
vitalets <vitalets@yandex-team.ru>
2018-12-25 19:35:52 +0300

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env'],
+            presets: ['@babel/preset-env'],
             plugins: babelPlugins,
           }
         }
